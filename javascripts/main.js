93c17d51b4a53c69982d58b8ef910ee26ffa6a5f
jleewebdev <lee45276@gmail.com>
2016-02-17 11:49:11 +0800

    Initial commit

---
+++ javascripts/main.js
@@ -0,0 +1,6 @@
+$(function(){
+
+  //Handlebars.compile($("[script*=x-handlebars-template"]).html());
+  //Handlebars.registerPartial("name", $([script*=x-handlebars-partial"]).html());
+  // $("div").html(template_function({key: value}));
+});
