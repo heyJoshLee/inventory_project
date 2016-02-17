dab40151cfeb53d8964257f81a2918c4faccbfc4
jleewebdev <lee45276@gmail.com>
2016-02-17 17:50:33 +0800

    Show edit_form when double clicking

--- javascripts/main.js
+++ javascripts/main.js
@@ -7,7 +7,10 @@ $(function(){
     templates[$(this).attr("id")] = Handlebars.compile($(this).html());
   });
 
-  console.log(templates);
+  function render() {
+    $("#inventoryItems").html(templates.inventoryItems({inventoryItems: inventoryItems}));
+  }
+
 
   Handlebars.registerPartial("inventoryItem", $("#inventoryItem").html());
 
@@ -18,8 +21,27 @@ $(function(){
       form_fields[$(this).attr("id")] = $(this).val();
     });
     inventoryItems.push(form_fields);
+    inventoryItems[inventoryItems.length - 1]["id"] = inventoryItems.length - 1;
+    render();
     this.reset();
-    $("#inventoryItems").html(templates.inventoryItems({inventoryItems: inventoryItems}));
+  });
+
+  $("table").on("click", ".delete_button", function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var check = confirm("Are you sure you want to delete " + inventoryItems[$this.attr("data-id")].name + " ?");
+    if (check) {
+      inventoryItems.splice([$this.attr("data-id"), 1]);
+      render();
+    }
+  });
+
+  $("table").on("dblclick", "tr", function(e) {
+    var $this = $(this);
+    var id = ($this.find("a").attr("data-id"));
+    var current_item = inventoryItems[id];
+    $this.html("");
+    $this.html(templates.editInventory(current_item));
   });
   //Handlebars.compile($("[script*=x-handlebars-template"]).html());
   //Handlebars.registerPartial("name", $([script*=x-handlebars-partial"]).html());
